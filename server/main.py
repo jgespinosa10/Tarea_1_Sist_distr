e864bcf54ebec69e393b3f1a79e5a42d22b660b6
Tomás Vergara <tomvergara@uc.cl>
2021-09-06 17:09:31 -0300

    feat(): ask for ngrok token

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 # Código recuperado de https://www.thepythoncode.com/article/make-a-chat-room-application-in-python
 from lib.helpers import process_args
 from lib.server import Server
+from lib.token import NGROK_TOKEN
 from pyngrok import ngrok
 
 if __name__ == "__main__":
@@ -12,6 +13,7 @@ if __name__ == "__main__":
     server = Server(n_clients, n_arg, SERVER_HOST, SERVER_PORT, separator_token)
 
     # Set up an ngrok tunnel to connect a public URL to localhost
+    ngrok.set_auth_token(NGROK_TOKEN)
     ssh_tunnel = ngrok.connect(SERVER_PORT, "tcp")
     print("URL:", ssh_tunnel.public_url.split('/')[-1].split(':')[0])
     print("PORT:", ssh_tunnel.public_url.split(':')[-1])
