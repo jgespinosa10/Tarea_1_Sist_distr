45b24e822efc9f840158e2b584c89303707a7af1
FloValdes <ftvaldes1@uc.cl>
2021-09-25 19:55:35 -0300

    fix pm not showing

--- client/lib/p2p.py
+++ client/lib/p2p.py
@@ -76,7 +76,7 @@ class P2P:
 
         self.send(skt, str(self.user.id))
         self.listen(skt)
-        if (msg[:2] == '0-'):
+        if (msg.split('-')[0] not in ['server', 'new-server']):
           print(msg)
         self.send(self.peer(id)['socket'], msg)
 
